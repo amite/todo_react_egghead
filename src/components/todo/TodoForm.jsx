a995b8defa313ccdbc42e7f8c6e999c5c5235d62
Amit Erandole <amit.erandole@gmail.com>
2017-01-31 09:32:45 +0530

    added proptypes for components

--- src/components/todo/TodoForm.jsx
+++ src/components/todo/TodoForm.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 
 export const TodoForm = (props) => (
-  <form action="">
+  <form>
     <input type="text" onChange={props.handleInputChange} 
     value={props.currentTodo} />
   </form>
 )
+
+TodoForm.propTypes = {
+  currentTodo: React.PropTypes.string.isRequired,
+  handleInputChange: React.PropTypes.func.isRequired
+}
